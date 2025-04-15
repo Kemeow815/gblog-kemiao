031bcaee0c516d2bb37ffe394c528d6eb6172ff1
克喵Kemeow <kemiaofx@163.com>
2025-04-16 02:13:30 +0800

    更新 AlgoliaDocSearch.tsx

--- src/components/support/search/AlgoliaDocSearch.tsx
+++ src/components/support/search/AlgoliaDocSearch.tsx
@@ -6,9 +6,9 @@ import './AlgoliaDocSearch.css'
 function AlgoliaDocSearch() {
     return (
         <DocSearch
-            appId="64IMEC5R2V"
-            indexName="gblog"
-            apiKey="6b2d146b29a701ba8ef6d617a0c1d4e1"
+            appId="IEETPPXHIW"
+            indexName="kemiaosw"
+            apiKey="b36afa7c543f62532af783c401e8d800"
         />
     )
 }
