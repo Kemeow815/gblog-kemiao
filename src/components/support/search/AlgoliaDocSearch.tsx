3d89fb1336159ed49c9aaf55ffc6578d60e53351
Kemeow815 <kemiao@kmblog.icu>
2025-04-17 12:14:05 +0800

    3

--- src/components/support/search/AlgoliaDocSearch.tsx
+++ src/components/support/search/AlgoliaDocSearch.tsx
@@ -6,15 +6,9 @@ import './AlgoliaDocSearch.css'
 function AlgoliaDocSearch() {
     return (
         <DocSearch
-<<<<<<< HEAD
-            appId="64IMEC5R2V"
-            indexName="gblog"
-            apiKey="7ac5c4186da007de510dea63b58d5973"
-=======
             appId="IEETPPXHIW"
             indexName="kemiaosw"
             apiKey="b36afa7c543f62532af783c401e8d800"
->>>>>>> 81e386e202d3c967b879e96b0239fcdff54079f8
         />
     )
 }
